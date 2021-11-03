9d028a8fea186d85865876513a63591acf9b1ef7
gchanet <gaelchanet@hotmail.com>
2021-11-03 10:10:37 +0100

    base

--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,73 @@
 /*   By: gchanet <gchanet@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 07:08:07 by gchanet           #+#    #+#             */
-/*   Updated: 2021/11/02 07:15:50 by gchanet          ###   ########lyon.fr   */
+/*   Updated: 2021/11/03 10:08:07 by gchanet          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/ft_printf.h"
 
+typedef struct s_printf
+{
+	va_list	ap;
+
+	int	flag_ptn:1; 		/* . */
+	int	flag_dash:1;		/* - */
+	int	flag_ast:1;			/* * */
+	int	flag_zero:1;		/* 0 */
+
+	int	conv_c:1;
+	int	conv_s:1;
+	int	conv_p:1;
+	int	conv_d:1;
+	int	conv_d:1;
+}			t_printf;
+
 int	ft_printf(const char *format, ...)
 {
+	int			pos;
+	t_printf	*ptf;
 
+	pos = 0;
+	*ptf = (t_printf *)ft_memalloc(sizeof(t_printf));
+	if (!ptf)
+		return (-1);
+	va_start(ptf->ap, format);
+	while (format[pos])
+	{
+	   if (format[pos] == '%')
+	   {
+			if (format[pos] == '.')
+			{
+				tab->pnt = 1; 
+				pos++;
+			}  
+			if (format[pos] == '-') 
+				tab->dash = 1; 
+			if (format[pos] == 'c') 
+				ft_print_char(tab); 
+			if (format[pos] == 'd' || format[pos] == 'i') 
+				ft_print_integer(tab); 
+				pos++; 
+	   }
+	   else
+	   {
+		   ft_putchar(format[pos]);
+	   }
+	   pos++;
+   }
+   va_end(ptf->ap);
 }
+/*
+c = va_arg(args, int) 
+s = va_arg(args, char *) 
+d = va_arg(args, int) 
+i = va_arg(args, int) 
+u = va_arg(args, unsigned int) 
+p = va_arg(args, unsigned long )# ou 
+                               #(unsigned long)va_arg(args, void *); 
+x = va_arg(args, entier non signé) 
+X = va_arg(args, entier non signé)
+*/
+
+//https://csnotes.medium.com/ft-printf-tutorial-42project-f09b6dc1cd0e
--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,11 +6,13 @@
 /*   By: gchanet <gchanet@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 07:07:53 by gchanet           #+#    #+#             */
-/*   Updated: 2021/11/02 07:08:03 by gchanet          ###   ########lyon.fr   */
+/*   Updated: 2021/11/03 09:11:29 by gchanet          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
+# include <stdarg.h>
+# include <stdio.h>
 
 #endif
